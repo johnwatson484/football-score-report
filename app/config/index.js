1211f4c35c9144024baed81dfe698a3d80f0d06c
John Watson <johnwatson484@hotmail.com>
2021-02-08 19:23:47 +0000

    Scram updated

--- app/config/index.js
+++ app/config/index.js
@@ -5,7 +5,7 @@ const config = {
     port: process.env.KAFKA_PORT || 9093,
     username: process.env.KAFKA_CLIENT_USER,
     password: process.env.KAFKA_CLIENT_PASSWORD,
-    mechanism: process.env.KAFKA_MECHANISM || 'scram-sha-256',
+    mechanism: process.env.KAFKA_MECHANISM || 'scram-sha-512',
     clientId: process.env.KAFKA_CLIENT_ID || 'football-score-report',
     topic: process.env.KAFKA_TOPIC || 'results'
   }
